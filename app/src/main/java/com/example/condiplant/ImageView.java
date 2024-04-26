8434de93d1e995b952bc38d5a88cabfa6cf34bc4
John Jomar <johndjomar@gmail.com>
2024-04-26 20:28:49 +0800

    Upload Image Function

---
+++ app/src/main/java/com/example/condiplant/ImageView.java
@@ -0,0 +1,4 @@
+package com.example.condiplant;
+
+public class ImageView {
+}
--- app/src/main/java/com/example/condiplant/MainActivity.java
+++ app/src/main/java/com/example/condiplant/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.condiplant;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnCapture, btnUpload, btnSeeDiseases;
+    ImageView imageView;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCapture = findViewById(R.id.btnCapture);
+        btnUpload = findViewById(R.id.btnUpload);
+        btnSeeDiseases = findViewById(R.id.btnSeeDiseases);
+        imageView = (ImageView) findViewById(R.id.imgView);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 10){
+            if (data != null){
+                Uri uri = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
