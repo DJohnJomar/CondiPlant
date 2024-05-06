da7cfcd9c9a0e581b81308c9f7da4920a8fe8a9c
John Jomar <johndjomar@gmail.com>
2024-05-06 15:21:20 +0800

    Added textview for prediction

--- app/src/main/java/com/example/condiplant/DisplayImageActivity.java
+++ app/src/main/java/com/example/condiplant/DisplayImageActivity.java
@@ -9,19 +9,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 
 public class DisplayImageActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    Button btnBack, btnRemedy;
+    private ImageView imageView;
+    private Button btnBack, btnRemedy;
+    private TextView txtPrediction;
     private String imagePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_image);
 
+        txtPrediction = findViewById(R.id.txtPrediction);
         imageView = findViewById(R.id.displayImageView);
 
 
