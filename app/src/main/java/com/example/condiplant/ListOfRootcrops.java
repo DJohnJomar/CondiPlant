b81f5327d8ebae4c19a69758b32576c8934b8ab5
James Meneses <james.meneses112603@gmail.com>
2024-09-03 14:26:50 +0800

    List of Disease about to be added

--- app/src/main/java/com/example/condiplant/ListOfRootcrops.java
+++ app/src/main/java/com/example/condiplant/ListOfRootcrops.java
@@ -1,6 +1,9 @@
 package com.example.condiplant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ListOfRootcrops extends AppCompatActivity {
+
+    private ImageButton btnBack, btnCassava, btnTaro, btnSweetPotato;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,41 @@ public class ListOfRootcrops extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnBack = findViewById(R.id.backButton);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();// Finish current activity and go back to previous activity (MainActivity)
+            }
+        });
+
+        btnCassava = findViewById(R.id.btnCassava);
+        btnCassava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListOfRootcrops.this, CassavaDiseases.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTaro = findViewById(R.id.btnTaro);
+        btnTaro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListOfRootcrops.this, TaroDiseases.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSweetPotato = findViewById(R.id.btnSweetPotato);
+        btnSweetPotato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListOfRootcrops.this, SweetPotatoDiseases.class);
+                startActivity(intent);
+            }
         });
     }
 }
