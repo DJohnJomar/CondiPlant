e9793804eacdc24f38a826d1cdaa6c98975fae7a
Mark Christian Banagan <markchrisbanagan@gmail.com>
2024-09-17 04:20:54 +0800

    Added Page Arrow Buttons as Indicator and Navigator

--- app/src/main/java/com/example/condiplant/HelpFragment.java
+++ app/src/main/java/com/example/condiplant/HelpFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
@@ -17,6 +19,7 @@ public class HelpFragment extends Fragment {
     ViewPager2 viewPager2;
     ArrayList<ViewPageItem> viewPageItemArrayList;
     ActivityMainBinding binding;
+    ImageButton prevPage, nextPage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +28,8 @@ public class HelpFragment extends Fragment {
         binding = ActivityMainBinding.inflate(inflater);
         View view = inflater.inflate(R.layout.fragment_help, container, false);
         viewPager2 = view.findViewById(R.id.viewPager);
+        prevPage = view.findViewById(R.id.prevPage);
+        nextPage = view.findViewById(R.id.nextPage);
 
         // Create ViewPageItem objects
         int[] images = {R.drawable.cassava_blight, R.drawable.cassava_blight, R.drawable.cassava_blight, R.drawable.cassava_blight};
@@ -44,6 +49,36 @@ public class HelpFragment extends Fragment {
         viewPager2.setClipChildren(false);
         viewPager2.setOffscreenPageLimit(2);
         viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                prevPage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewPager2.setCurrentItem(position - 1);
+                    }
+                });
+                nextPage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewPager2.setCurrentItem(position + 1);
+                    }
+                });
+
+                if (position == 0){
+                    prevPage.setVisibility(View.INVISIBLE);
+                    nextPage.setVisibility(View.VISIBLE);
+                } else if (position == viewPageItemArrayList.size() - 1){
+                    prevPage.setVisibility(View.VISIBLE);
+                    nextPage.setVisibility(View.INVISIBLE);
+                } else {
+                    prevPage.setVisibility(View.VISIBLE);
+                    nextPage.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         DotsIndicator dotsIndicator = view.findViewById(R.id.dots_indicator);
         dotsIndicator.setViewPager2(viewPager2);
 
