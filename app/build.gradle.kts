7839c23603d6fe3bb13ae69f6a8257adbf228bfd
John Jomar Dimaunahan <johndjomar@gmail.com>
2024-09-30 19:48:31 +0800

    Added comments to separate the toolbar clearly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.vanniktech:android-image-cropper:4.6.0")
     implementation ("com.google.android.material:material:1.4.0")
+    implementation ("com.tbuonomo:dotsindicator:4.2")
 }
