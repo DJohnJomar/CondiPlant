75ee3982a31f19a6c47b9de3c0081b0fe11250c3
Mark Christian Banagan <markchrisbanagan@gmail.com>
2024-10-29 18:39:55 +0800

    Added Splash Screen

---
+++ app/src/main/java/com/example/condiplant/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.condiplant;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+private ImageView splashImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setSupportActionBar(null);
+
+        splashImageView = findViewById(R.id.splashImageView);
+        splashImageView.setImageResource(R.drawable.splash_image);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
