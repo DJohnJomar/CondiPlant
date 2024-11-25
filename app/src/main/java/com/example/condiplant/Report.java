c8b8240233d69de2a8bafae88a19d0f0b90b9442
John Jomar Dimaunahan <johndjomar@gmail.com>
2024-11-25 18:14:35 +0800

    Added initial layout for date picker (Not working) and recyclerview)

--- app/src/main/java/com/example/condiplant/Report.java
+++ app/src/main/java/com/example/condiplant/Report.java
@@ -1,8 +1,13 @@
 package com.example.condiplant;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class Report extends AppCompatActivity {
 
     private ImageButton btnBack;
+    private Button btnDatePicker;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,7 @@ public class Report extends AppCompatActivity {
             return insets;
         });
 
+
         btnBack = findViewById(R.id.backButton);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +42,83 @@ public class Report extends AppCompatActivity {
                 finish();// Finish current activity and go back to previous activity (MainActivity)
             }
         });
+        initDatePicker();
+
+        btnDatePicker = findViewById(R.id.btnDatePicker);
+        btnDatePicker.setText(getTodaysDate());
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private String getTodaysDate(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        month = month + 1; // Month is set to 0 (January is 0). This is to set it to 1 as january is in the calendar
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                month = month + 1; // Month is set to 0 (January is 0). This is to set it to 1 as january is in the calendar
+                String date = makeDateString(day, month, year);
+                btnDatePicker.setText(date);
+
+            }
+        };
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year , month, day);
+
+    }
+
+    private String makeDateString(int day, int month, int year){
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat (int month){
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+        //Default should never happen
+        return "JAN";
+    }
+
+    public void openDatePicker(View view){
+        datePickerDialog.show();
     }
 }
